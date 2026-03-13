81c9169c190f0090f2e32d8674ea121423d02ef0
dkswlsdn <dkswlsdn12@gmail.com>
2026-03-13 18:01:13 +0900

    C# 확장 메서드 만들기 - 중간
    Chunk 기능 구현?
    yield return?

---
+++ Chunk/Chunk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class ChunkClass
+{
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> list, int size)
+    {
+        List<List<T>> lists = new List<List<T>>();
+        List<T> tempList = list.ToList();
+
+        int count = 0;
+        bool isEnd = false;
+
+        while (!isEnd)
+        {
+            List<T> temp = new List<T>();
+
+            for (int i = size * count; i < size * count + size; i++)
+            {
+                if (tempList.Count == i)
+                {
+                    isEnd = true;
+                    break;
+                }
+
+                temp.Add(tempList[i]);
+            }
+
+            lists.Add(temp);
+
+            count++;
+        }
+
+        return lists;
+    }
+}
--- Chunk/Program.cs
+++ Chunk/Program.cs
@@ -2,5 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+int[] intList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+string[] students = { "김철수", "이영희", "박민수", "최지연", "정우진" };
+int[] pages = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 2, 23, 24, 25 };
+
+
+Console.WriteLine("=== 컬렉션 청킹 테스트 ===");
+
+Console.WriteLine("[숫자를 3개씩 그룹화]");
+
+foreach (var chunk in intList.Chunk<int>(3))
+{
+    Console.Write('[');
+
+    Console.Write(string.Join(", ", chunk));
+
+    Console.WriteLine(']');
+}
+
+
+Console.WriteLine("\n[학생들을 2명씩 팀 구성]");
+
+foreach (var chunk in students.Chunk<string>(2))
+{
+    Console.Write(string.Join(", ", chunk));
+    Console.WriteLine();
+}
